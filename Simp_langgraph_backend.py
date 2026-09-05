80bcd4ad86c2883c199915ba4209cb6c88735fbd
AdiAntasJain2006 <adiantasjain724@gmail.com>
2026-09-06 03:36:48 +0530

    Added sidebar in frontend with capability of new chat and also added database checkpointer

--- Simp_langgraph_backend.py
+++ Simp_langgraph_backend.py
@@ -1,10 +1,11 @@
 from langgraph.graph import START, END, StateGraph
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langchain_ollama import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage
+import sqlite3
 
 llm = ChatOllama(model="llama3.2:3b") 
 
@@ -17,7 +18,9 @@ def chat_node(state : ChatState) -> ChatState :
     response = llm.invoke(full_message)
     return {"message" : [response]}
 
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database="AAJ_database.db", check_same_thread=False)
+
+ck_pointer = SqliteSaver(conn = conn) # we use it because after completing this code one time chatstate gets empty so we store its last state in our memory using this checkpointer
 
 graph = StateGraph(ChatState)
 
@@ -25,4 +28,19 @@ graph.add_node("ChatNode", chat_node)
 graph.add_edge(START, "ChatNode")
 graph.add_edge("ChatNode", END)
 
-chatbot = graph.compile(checkpointer=checkpointer)
+chatbot = graph.compile(checkpointer=ck_pointer)
+
+# result = chatbot.invoke(
+#                 {"message" : [HumanMessage(content="Hello buddy my name is adi")]},
+#                 config={"configurable" : {"thread_id" : "thread_1"}})
+
+# for checkpoint in ck_pointer.list(None) :
+#     print(checkpoint.config["configurable"]["thread_id"])
+
+def retrieve_all_threads() :
+    all_threads = set()
+    for checkpoint in ck_pointer.list(None) :
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+        
+    return list(all_threads)
+    
--- Streamlit_frontend.py
+++ Streamlit_frontend.py
@@ -1,12 +1,70 @@
 import streamlit as st
 from Simp_langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+from Simp_langgraph_backend import retrieve_all_threads
+import uuid
+
+
+
+def create_thread_id() : # creates random thread_id
+    th_id = uuid.uuid4()
+    return th_id
+
+def add_thread(thread_id) : # add thread_id to chat_threads list where all threads are present 
+    if thread_id not in st.session_state["chat_threads"] :
+        st.session_state["chat_threads"].append(thread_id)
+
+def reset_chat() : # if we create new chat then it is used
+    th_id = create_thread_id()
+    st.session_state["thread_id"] = th_id
+    add_thread(st.session_state["thread_id"])
+    st.session_state["message_hist"] = []
+    
+def load_conv(thread_id) : # load conversation from chatbot
+    return chatbot.get_state({"configurable" : {"thread_id" : thread_id}}).values["message"]
+    
+
 
 # we will use streamlit inbuilt dictionary for message history storage which is {session_state -> dict}
-st.title("LangGraph Ollama Chatbot")
+st.title("AAJ's Chatbot")
 
 if "message_hist" not in st.session_state :
     st.session_state["message_hist"] = []
+    
+if "thread_id" not in st.session_state :
+    st.session_state["thread_id"] = create_thread_id()
+    
+if "chat_threads" not in st.session_state :
+    st.session_state["chat_threads"] = retrieve_all_threads()
+    
+add_thread(st.session_state["thread_id"])
+    
+with st.sidebar:
+    st.title("AAJ Chatbot")
+    
+    # Start chat button
+    if st.button("New Chat", type="primary", use_container_width=True):
+        reset_chat()
+    
+    st.markdown("---")
+    st.header("Chat History")
+    
+    for thread_id in reversed(st.session_state["chat_threads"]) :
+        if st.button(str(thread_id)) :
+            st.session_state["thread_id"] = thread_id
+            messages = load_conv(thread_id)
+            
+            temp_messages = []
+            for message in messages :
+                if isinstance(message, HumanMessage): # isinstance(object, class): A built-in Python function that returns True if the object matches the class, and False otherwise
+                    role = "user"
+                else :
+                    role = "assistant"
+                
+                temp_messages.append({"role" : role, "content" : message.content})
+            
+            st.session_state["message_hist"] = temp_messages
+            
 
 for message in st.session_state["message_hist"] :
     with st.chat_message(message["role"]) :
@@ -17,14 +75,14 @@ input = st.chat_input("Please enter query")
 if input :  
     # adding message to history
     st.session_state["message_hist"].append({"role" : "user", "content" : input})
-    with st.chat_message('user') :
+    with st.chat_message('user') : # here we just add user animated block in our frontend 
         st.write(input)
     
-    with st.chat_message("assistant") :
+    with st.chat_message("assistant") : # here we just add assistant animated block in our frontend
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream(
                 {"message" : [HumanMessage(content=input)]},
-                config={"configurable" : {"thread_id" : "id_1"}}, 
+                config={"configurable" : {"thread_id" : st.session_state["thread_id"]}}, 
                 stream_mode="messages")
         )
     
